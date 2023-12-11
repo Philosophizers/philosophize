27f302f83e42982ccc237c8dc53df61ff2ba6ce0
Yoseph <yoslatif@gmail.com>
2023-12-10 22:03:29 -0800

    seedfix

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,14 +1,14 @@
-# from flask.cli import AppGroup
-# from .users import seed_users, undo_users
+from flask.cli import AppGroup
+from .users import seed_users, undo_users
 
-# from app.models.db import db, environment, SCHEMA
+from app.models.db import db, environment, SCHEMA
 
 # # Creates a seed group to hold our commands
 # # So we can type `flask seed --help`
 # seed_commands = AppGroup('seed')
 
 
-# # Creates the `flask seed all` command
+# Creates the `flask seed all` command
 # @seed_commands.command('all')
 # def seed():
 #     if environment == 'production':
@@ -18,14 +18,14 @@
 #         # Make sure to add all your other model's undo functions below
 #         undo_users()
 #     seed_users()
-#     # Add other seed functions here
+    # Add other seed functions here
 
 
-# # Creates the `flask seed undo` command
+# Creates the `flask seed undo` command
 # @seed_commands.command('undo')
 # def undo():
 #     undo_users()
-#     # Add other undo functions here
+    # Add other undo functions here
 
 
 from flask.cli import AppGroup
@@ -41,6 +41,8 @@ seed_commands = AppGroup("seed")
 
 @seed_commands.command("all")
 def seed():
+    if environment == 'production':
+        undo()
     seed_users()
     seed_topics()
     seed_comments()
