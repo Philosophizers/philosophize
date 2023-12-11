beb0a800d909e25fe4da0dec16fd992201e4bff5
Yoseph <yoslatif@gmail.com>
2023-12-11 03:16:48 -0800

    comment2

--- app/models/comment.py
+++ app/models/comment.py
@@ -2,12 +2,13 @@
 
 from .db import db
 from datetime import datetime
+from user import User
 
 class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id), nullable=False)
     topic_id = db.Column(db.Integer, db.ForeignKey('topics.id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
