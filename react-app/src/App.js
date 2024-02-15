b7c9a9266cbf6700ed27dbe0e6efc5fc407172d3
Yoseph <yoslatif@gmail.com>
2024-02-15 04:13:32 -0800

    plspls

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import SignupFormModal from "./components/SignupFormModal";
 import LoginFormModal from "./components/LoginFormModal"; 
 import { authenticate } from "./store/session";
