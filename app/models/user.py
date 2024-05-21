e7849d7b3bc9ba7c7e6b2ff7c10ddcd9c0592e8a
yoslatif <yoslatif@gmail.com>
2024-05-20 22:42:22 -0700

    ok

--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,9 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    topics = db.relationship('Topic', backref='user', cascade="all, delete-orphan")
+    comments = db.relationship('Comment', backref='user', cascade="all, delete-orphan")
+
     @property
     def password(self):
         return self.hashed_password
